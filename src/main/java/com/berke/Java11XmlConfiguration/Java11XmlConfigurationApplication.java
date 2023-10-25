4bf187c9e12ef5b1fe9ca85016922ff70ec7120c
Berke <r.berkeyildirimer@gmail.com>
2023-10-25 09:49:35 +0300

    Initial commit

---
+++ src/main/java/com/berke/Java11XmlConfiguration/Java11XmlConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.berke.Java11XmlConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11XmlConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11XmlConfigurationApplication.class, args);
+	}
+
+}
